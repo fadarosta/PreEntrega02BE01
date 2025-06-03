f2b5813f36455b5d49f5e05eaca7fc249be9c845
Fabio Rostagno <113780343+fadarosta@users.noreply.github.com>
2025-06-02 21:00:21 -0300

    unexpected

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
+// app.js
 import express from 'express';
 import { engine } from 'express-handlebars';
 import productRouter from './src/routers/products.router.js';
 import cartRouter from './src/routers/carts.router.js';
 import errorHandler from './middlewares/errorHandler.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views')
-app.set('views engine', 'handlebars')
-app.use(express.static(__dirname + '/views'))
+app.engine(
+  'handlebars',
+  engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    extname: '.handlebars'
+  })
+);
 
-app.get('/', (req, res)=>
-    const testUser={
-        name: "BackEnd01"
-        last_name: "2025"
-    }
-    res.render('index', testUser)
-)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  const testUser = {
+    name: 'BackEnd01',
+    last_name: '2025'
+  };
+  res.render('index', testUser);
+});
 
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
+
 app.use(errorHandler);
 
-app.listen(8080, () => {
-    console.log('Servidor escuchando en el puerto 8080');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
